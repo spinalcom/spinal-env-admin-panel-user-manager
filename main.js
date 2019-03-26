8421c23bc966a14d646384391227bccc4b98d89f
laurent <htd.laurent@gmail.com>
2019-03-26 14:25:56 +0100

    use spinal-browser-user_role_manager

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-require("./SpinalUserManagerService");
-require("./UserManagerCtrl");
+// require("./SpinalUserManagerService");
+// require("./UserManagerCtrl");
 (function() {
+  window.angular.module("app.spinal-panel").controller("userManagerPanelIframeCtrl",
+    [
+      function() {}
+    ]);
+
+
   window.angular.module("app.spinal-panel").run([
     "$templateCache",
     "$http",
@@ -16,22 +22,25 @@ require("./UserManagerCtrl");
           }
         );
       };
-      let toload = [
-        {
-          uri:
-            "../templates/spinal-env-admin-panel-user-manager/user-manager-panel.html",
-          name: "user-manager-panel.html"
-        },
-        {
-          uri:
-            "../templates/spinal-env-admin-panel-user-manager/user-manager-create-user.html",
-          name: "user-manager-create-user.html"
-        },
-        {
-          uri:
-            "../templates/spinal-env-admin-panel-user-manager/user-manager-edit.html",
-          name: "user-manager-edit.html"
-        }
+      let toload = [{
+        uri: "../templates/spinal-env-admin-panel-user-manager/user-manager-panel-iframe.html",
+        name: "user-manager-panel-iframe.html"
+      }
+        // {
+        //   uri:
+        //     "../templates/spinal-env-admin-panel-user-manager/user-manager-panel.html",
+        //   name: "user-manager-panel.html"
+        // },
+        // {
+        //   uri:
+        //     "../templates/spinal-env-admin-panel-user-manager/user-manager-create-user.html",
+        //   name: "user-manager-create-user.html"
+        // },
+        // {
+        //   uri:
+        //     "../templates/spinal-env-admin-panel-user-manager/user-manager-edit.html",
+        //   name: "user-manager-edit.html"
+        // }
       ];
       for (var i = 0; i < toload.length; i++) {
         load_template(toload[i].uri, toload[i].name);
@@ -39,20 +48,37 @@ require("./UserManagerCtrl");
 
       goldenLayoutService.registerPanel({
         id: "drag-users-list-panel",
-        name: "Users List",
+        name: "User Manager",
         cfg: {
           isClosable: true,
-          title: "Users List",
+          title: "User Manager",
           type: "component",
           width: 20,
           componentName: "SpinalHome",
           componentState: {
-            template: "user-manager-panel.html",
+            template: "user-manager-panel-iframe.html",
             module: "app.spinal-panel",
-            controller: "UserManagerCtrl"
+            controller: "userManagerPanelIframeCtrl"
           }
         }
       });
+
+      // goldenLayoutService.registerPanel({
+      //   id: "drag-users-list-panel",
+      //   name: "Users List",
+      //   cfg: {
+      //     isClosable: true,
+      //     title: "Users List",
+      //     type: "component",
+      //     width: 20,
+      //     componentName: "SpinalHome",
+      //     componentState: {
+      //       template: "user-manager-panel.html",
+      //       module: "app.spinal-panel",
+      //       controller: "UserManagerCtrl"
+      //     }
+      //   }
+      // });
     }
   ]);
 })();
